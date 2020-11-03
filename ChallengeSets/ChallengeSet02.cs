cf765565dba7413d903920f43e56ab690f261241
Bryce <ImperialEagle1998@gmail.com>
2020-11-03 12:12:35 -0600

    Challenge 2 completed.

--- ChallengeSets/ChallengeSet02.cs
+++ ChallengeSets/ChallengeSet02.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
 
 namespace ChallengeSets
 {
@@ -7,52 +10,192 @@ namespace ChallengeSets
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            if (c != 'c')
+            {
+                return false;
+
+
+            }
+
+            else
+            {
+                return true;
+            }
+
+
+
+
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            if (vals.Length % 2 != 0)
+            {
+                return false;
+            }
+
+            else
+            {
+                return true;
+            }
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            if (number % 2 != 0)
+            {
+                return false;
+
+            }
+
+            else
+            {
+                return true;
+            }
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            if (num % 2 == 0)
+            {
+
+                return false;
+            }
+
+            else
+            {
+                return true;
+            }
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+
+            if (numbers == null)
+            {
+                return 0;
+            }
+            if(numbers.Count() == 0)
+            {
+                return 0;
+            }
+
+
+            else
+            {
+                return numbers.Min() + numbers.Max();
+            }
+
+
+
+
+
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            if (str1.Length < str2.Length)
+            {
+                int answer = str1.Length;
+                return answer;
+            }
+            else
+            {
+                int answer2 = str2.Length;
+                return answer2;
+            }
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            var ans = 0;
+            if (numbers == null)
+            {
+                return 0;
+            }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] <= numbers.Length)
+                {
+                    ans += numbers[i];
+                }
+            }
+            return ans;
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            var ans = 0;
+            if (numbers == null)
+            {
+                return 0;
+            }
+
+           
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    ans += numbers[i];
+                }
+            }
+            return ans;
         }
+    
+
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            int total = 0;
+            if (numbers == null)
+            {
+                return false;
+            }
+
+            for (int i = 0;i < numbers.Count; i++)
+            {
+
+                total += numbers[i];
+
+            }
+
+            if(total % 2 == 0)
+            {
+                return false;
+            }
+             else
+            {
+                return true;
+            }
+
+
         }
+          
+        
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            var answer = 0;
+          if(number <= 0)
+            {
+
+                return 0;
+            }
+            
+            for(int i = 0;i < number;i++)
+            {
+                if (i % 2 != 0)
+                {
+                    answer++;
+                }
+               
+               
+            }
+            return answer;
+           
+
+
         }
     }
 }
