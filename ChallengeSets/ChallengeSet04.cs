6cd0fbeb24a35396339efde06fc839c3ca195446
Bryce <ImperialEagle1998@gmail.com>
2020-12-05 11:39:17 -0600

    Finished 4th challenge, forgot to send last night.

--- ChallengeSets/ChallengeSet04.cs
+++ ChallengeSets/ChallengeSet04.cs
@@ -1,51 +1,166 @@
 ﻿using System;
+using System.Linq;
 namespace ChallengeSets
 {
     public class ChallengeSet04
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+
+            int OddSum = numbers.ToList().Where(i => i % 2 != 0).Sum();
+            int EvenSum = numbers.ToList().Where(i => i % 2 == 0).Sum();
+            int answer = 0;
+            for(int i = 0;i <= numbers.Length; i++)
+            {
+                return EvenSum - OddSum;
+
+
+                
+            }
+
+            return answer;
+
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            var arrofstrings = new string[] { str1, str2, str3, str4 };
+
+            var shorteststring = arrofstrings.OrderBy(str => str.Length).FirstOrDefault();
+
+            return shorteststring.Length;
+
+
+
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            var arrayofvalues = new int[] { number1, number2, number3, number4 };
+
+            var smallestvalue = arrayofvalues.Min();
+
+            return smallestvalue;
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+           
+                biz.Name = "TrueCoders";
+
+
+            
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+
+            if (sideLength1 > 0 & sideLength2 > 0 && sideLength1 + sideLength2 > sideLength3 && sideLength1 + sideLength3 > sideLength2 && sideLength2 + sideLength3 > sideLength1 )
+            {
+                return true;
+            
+
+            }
+             else
+            {
+                return false;
+            }
+     
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+          
+            decimal randII = 1;
+           
+            var test2 = decimal.TryParse(input, out randII);
+            return test2;
+          
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+          
+            int numberofnulls = 0;
+            int numberofvalues = 0;
+            var objectlength = objs.Length / 2;
+
+            foreach (object test in objs)
+            {
+              
+              
+                if(test == null)
+                {
+                    numberofnulls++;
+                 
+                }
+
+              
+
+                
+               
+
+
+
+            }
+            if(numberofnulls > objectlength)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+
+      
+
+
         }
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
-        }
+         
+            double sumcounter = 0;
+            double sum = 0;
+            
+            if(numbers == null)
+            {
+                return 0;
+            }
+            
+            for(int i = 0;i < numbers.Length; i++)
+            { 
 
+                if (numbers[i] % 2 == 0)
+                {
+                    sumcounter++;
+                    sum += numbers[i];
+
+                }               
+            }
+          if(sumcounter == 0 )
+            {
+                return 0;
+            }   
+          
+            return sum / sumcounter;      
+        }
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+
+            int count = 1;
+            int result = 1;
+            
+
+            while(count < number)
+            {
+                count++;
+                result *= count;
+
+
+            }
+            return result;
         }
     }
 }
