7a1bd83faeff60e462fcb96fa6819c08529c7d98
Bryce <ImperialEagle1998@gmail.com>
2021-02-08 00:15:28 -0600

    Studied answers, so simple when You really look at it.

--- ChallengeSets/ChallengeSet07.cs
+++ ChallengeSets/ChallengeSet07.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengeSets
 {
@@ -7,29 +8,108 @@ namespace ChallengeSets
     {
         public int CountOfBusinessesWithNegativeNetProfit(List<Business> businesses)
         {
-            throw new NotImplementedException();
+            if (businesses == null || businesses.Count == 0)
+            {
+                return 0;
+            }
+
+            var count = 0;
+
+            foreach (var biz in businesses)
+            {
+                if ((biz.TotalRevenue - biz.TotalExpenses) < 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
         }
 
         public string GetCommaSeparatedListOfProfitableBusinesses(List<Business> businesses)
         {
-            throw new NotImplementedException();
+            var profitableBusinesses = new List<string>();
+
+            foreach (var biz in businesses)
+            {
+                var profit = biz.TotalRevenue - biz.TotalExpenses;
+
+                if (profit > 0)
+                {
+                    profitableBusinesses.Add(biz.Name);
+                }
+            }
+
+            return string.Join(',', profitableBusinesses);
         }
 
         public string GetNameOfHighestParentCompany(Business business)
         {
             // If there is Company A, whose parent is Company B, whose parent is Company C, then given Company A return Company C
-            throw new NotImplementedException();
+            Business company = business;
+
+            while (company.ParentCompany != null)
+            {
+                company = company.ParentCompany;
+            }
+
+            return company.Name;
         }
 
         public enum TicTacToeResult { X, O, Draw }
         public TicTacToeResult GetTicTacToeWinner(char[,] finalBoard)
         {
-            throw new NotImplementedException();
-        }
+            var backDiagonal = new List<char>();
 
+            backDiagonal.Add(finalBoard[0, 2]);
+            backDiagonal.Add(finalBoard[1, 1]);
+            backDiagonal.Add(finalBoard[2, 0]);
+
+            for (int y = 0; y < 3; y++)
+            {
+                var vertLine = new List<char>();
+                var horzLine = new List<char>();
+                var fordDiagonal = new List<char>();
+
+                for (int x = 0; x < 3; x++)
+                {
+                    vertLine.Add(finalBoard[x, y]);
+                    horzLine.Add(finalBoard[y, x]);
+                    fordDiagonal.Add(finalBoard[x, x]);
+                }
+
+                if (string.Join("", vertLine) == "XXX" || string.Join("", horzLine) == "XXX" || string.Join("", fordDiagonal) == "XXX" || string.Join("", backDiagonal) == "XXX")
+                {
+                    return TicTacToeResult.X;
+                }
+                var z = string.Join("", vertLine);
+
+                if (string.Join("", vertLine) == "OOO" || string.Join("", horzLine) == "OOO" || string.Join("", fordDiagonal) == "OOO" || string.Join("", backDiagonal) == "OOO")
+                {
+                    return TicTacToeResult.O;
+                }
+            }
+            return TicTacToeResult.Draw;
+        }
         public bool EachArrayInJaggedArrayContainsTargetNumber(int[][] numbers, int targetNumber)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0)
+            {
+                return false;
+            }
+
+
+            bool flag = true;
+
+            foreach (var collect in numbers)
+            {
+                if (!collect.Contains(targetNumber))
+                {
+                    flag = false;
+                }
+            }
+
+            return flag;
         }
     }
 }
